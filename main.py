3cec19e6f55200437a8c2f1b2c0783088e35c1dd
fireing123 <gimd82368@gmail.com>
2024-03-11 13:44:30 +0900

    경로 안정화

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
+import os
 from pygame_phyics import *
+from pygame_phyics import game
+
+os.chdir(
+    os.path.abspath(os.path.dirname(__file__))
+)
 
 SCREEN_SIZE = (1200, 800)
 TITLE = "title"
 
 Game.init(SCREEN_SIZE, TITLE)
+
 
 Game.import_objects("objects/", debug="log")
 
